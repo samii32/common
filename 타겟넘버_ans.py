b94d695e18d9ff29a74a504281a14054e6ff9ddd
sam <smile_666@naver.com>
2020-12-08 11:14:43 +0900

    target_number ans

--- 타겟넘버_ans.py
+++ 타겟넘버_ans.py
@@ -2,7 +2,7 @@ target = 3
 numbers = [1,1,1,1,1]
 
 #dp처럼
-# sol(numbers, target) : target을 만족시키는 갯수
+# sol(numbers, target) : numbers를 가지고 target을 만족시키는 갯수
 # sol(numbers, target) = sol(numbers[1:],target-numbers[0])+sol(numbers[1:],target+numbers[0])
 def sol(numbers, target):
     if not numbers and target == 0:
