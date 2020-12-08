23df5f28b02dcc714919554d50a98f5a7c354add
sam <smile_666@naver.com>
2020-12-08 11:11:39 +0900

    target_number ans

---
+++ 타겟넘버_ans.py
@@ -0,0 +1,14 @@
+target = 3
+numbers = [1,1,1,1,1]
+
+#dp처럼
+# sol(numbers, target) : target을 만족시키는 갯수
+# sol(numbers, target) = sol(numbers[1:],target-numbers[0])+sol(numbers[1:],target+numbers[0])
+def sol(numbers, target):
+    if not numbers and target == 0:
+        return 1
+    elif not numbers:
+        return 0
+    return sol(numbers[1:],target-numbers[0])+sol(numbers[1:],target+numbers[0])
+
+print(sol(numbers,target))
